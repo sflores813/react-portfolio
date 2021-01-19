b494a7114e4d5d03da3765c561ecf9a196b2dbe7
Stephen Flores <sflores813@gmail.com>
2021-01-18 23:52:09 -0600

    alot

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,14 +16,14 @@ class Footer extends Component {
         <div className="twelve columns">
            <ul className="social-links">
               {networks}
-           </ul>
+           {/* </ul>
 
            <ul>
             <li>Modified with love by the Clever Programmer team ♥️</li>
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
+              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li> */}
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
